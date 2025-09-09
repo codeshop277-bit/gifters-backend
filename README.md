a600d7154fcae5225d86c4bda17b05c5d9f4493d
Codeshop277 <codeshop277@gmail.com>
2025-09-09 11:53:45 +0530

    Add columns to existing tables

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # gifters-backend
+
+Add columns to exisiting tables.
+alembic revision --autogenerate -m "Add hashed password column to users table"
+alembic upgrade head
--- models.py
+++ models.py
@@ -19,7 +19,7 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String(150), unique=True, nullable=False)
-    #hashed_password = Column(String(255), nullable=False)
+    hashed_password = Column(String(255), nullable=True)
     password = Column(String(200), nullable=False)
     name = Column(String(100), nullable=False)
 
