f1c8c0ceab30221bb4d778ef523e1aac0006a1a9
Codeshop277 <codeshop277@gmail.com>
2026-04-04 11:53:14 +0530

    main changes

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ init_db()
 app.include_router(gifts.router)
 app.include_router(users.router)
 app.include_router(share.router)
-
+#app.include_router(auths.router)
 @app.get("/", tags=["Root"])
 def read_root():
-    return {"message": "Gifters backend is running successfully 🎉"}
+    return {"message": "Gifters backend is running successfully"}
